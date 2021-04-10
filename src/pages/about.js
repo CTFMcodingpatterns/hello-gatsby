bd097727ac49c430a548d1536f082552444d26ac
CTFM <pipapopos@yahoo.de>
2021-04-10 10:10:55 +0200

    about add links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
+        <Link to="/">Home</Link> |
+        <Link to="/contact/">Contact</Link> | 
+        <Link to="/about/">About</Link>
         <Header headerText="about gatsby" phrase="is arbitrary" />
         <Header headerText="It's pretty cool" />
       {/* <h1>About Gatsby</h1> */}
